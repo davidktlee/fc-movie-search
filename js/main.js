20c08e4fbeb68744c3edd4039945c25e75188a9a
davidktlee <rudxor567@naver.com>
2022-05-10 17:31:29 +0900

    feat: Create more button and animation.
    more 버튼을 누르면 다음 페이지가 계속 검색 될 수 있도록 했고
    
    main.js 파일에서 함수를 역할별로 나눠 보았다.

--- js/main.js
+++ js/main.js
@@ -2,18 +2,39 @@
 import '../scss/main.scss'
 
 const containerEl = document.querySelector('.movie-container')
-const btnEl = document.querySelector('.material-symbols-outlined')
+const searchBtnEl = document.querySelector('.material-symbols-outlined')
 const inputEl = document.querySelector('input')
+const moreBtnEl = document.createElement('button')
+const mainInnerEl = document.querySelector('.main .inner')
+const resultEl = document.createElement('div')
 
-btnEl.addEventListener('click', () => {
-  containerEl.innerHTML = ''
-  getMovie(inputEl.value)
-})
+const API_KEY = 'apikey=7035c60c'
 
-async function getMovie(title) {
-  let res = await fetch(`https://www.omdbapi.com?apikey=7035c60c&s=${title}`)
+async function getMovie(title, page) {
+  let res = await fetch(`https://www.omdbapi.com?${API_KEY}&s=${title}&page=${page}`)
   res = await res.json()
   const datas = res.Search
+  const totalRes = res.totalResults
+  renderMovies(datas, totalRes, title)
+}
+
+const searchMovies = () => {
+  let page = 1
+  searchBtnEl.addEventListener('click', () => {
+    containerEl.innerHTML = ''
+    resultEl.textContent = ''
+    getMovie(inputEl.value, page)
+    inputEl.value = ''
+  })
+
+  moreBtnEl.addEventListener('click', () => {
+    page += 1
+    getMovie(inputEl.value, page)
+  })
+}
+searchMovies()
+
+const renderMovies = (datas, totalRes, title) => {
   datas.forEach((data) => {
     const divEl = document.createElement('div')
     const titleEl = document.createElement('div')
@@ -27,4 +48,11 @@ async function getMovie(title) {
     divEl.appendChild(imgEl)
     containerEl.appendChild(divEl)
   })
+  moreBtnEl.textContent = 'More'
+  moreBtnEl.classList.add('moreBtn')
+  containerEl.appendChild(moreBtnEl)
+
+  resultEl.classList.add('resultDiv')
+  resultEl.textContent = `${title}의 총 검색 결과: ${totalRes}개`
+  mainInnerEl.prepend(resultEl)
 }
