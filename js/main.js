e979173bb047270e899d56e5cf570fc50b5876df
davidktlee <rudxor567@naver.com>
2022-05-12 17:59:48 +0900

    feat: Create loading image and error.
    setTimeout 함수는 promise 객체를 반환하지 않기 때문에 try catch 문으로
    받을 수가 없다.
    쓰려면 위처럼 promise 객체를 반환하게 만들어 변수에 담고 사용해야 한다.
    
    비동기 에 대해서 더 공부해야 될 것 같다

--- js/main.js
+++ js/main.js
@@ -9,16 +9,58 @@ const mainInnerEl = document.querySelector('.main .inner')
 const resultEl = document.createElement('div')
 const sortEl = document.querySelector('select')
 const optionValue = sortEl.options[sortEl.selectedIndex].value
+const errorContainerEl = document.createElement('div')
+const loadingEl = document.createElement('img')
 
 const API_KEY = 'apikey=7035c60c'
 
+// 영화 불러오기
 async function getMovie(title, page) {
-  let res = await fetch(`https://www.omdbapi.com?${API_KEY}&s=${title}&page=${page}`)
-  res = await res.json()
-  const datas = res.Search
-  const totalRes = res.totalResults
-  renderMovies(datas, totalRes, title)
+  loadingFunc()
+  try {
+    let res = await fetch(`https://www.omdbapi.com?${API_KEY}&s=${title}&type=movie&page=${page}`)
+    // setTimeout
+    const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay))
+    await wait(1000)
+    res = await res.json()
+    const datas = res.Search
+    const totalRes = res.totalResults
+    renderMovies(datas, totalRes, title)
+  } catch (error) {
+    renderError(error)
+  }
 }
+// 에러 시 호출 함수
+const renderError = (error) => {
+  loadingEl.classList.remove('viewImg')
+  loadingEl.classList.add('hideImg')
+  errorContainerEl.classList.remove('hide')
+  errorContainerEl.classList.add('error')
+  errorContainerEl.innerHTML = `<span>404 Error</span><br /><span>찾는 영화가 없습니다ㅠ^ㅠ</span>`
+  mainInnerEl.appendChild(errorContainerEl)
+}
+// 로딩 함수
+const loadingFunc = () => {
+  loadingEl.classList.remove('hideImg')
+  loadingEl.classList.add('viewImg')
+  loadingEl.src = './KakaoTalk_20220503_144117188.jpg'
+  mainInnerEl.appendChild(loadingEl)
+  errorContainerEl.innerHTML = ''
+}
+
+// 시리즈 불러오기 구현 할 것
+// async function getSeries(title, page) {
+//   try {
+//     let res = await fetch(`https://www.omdbapi.com?${API_KEY}&s=${title}&type=series&page=${page}`)
+//     res = await res.json()
+//     const datas = res.Search
+//     const totalRes = res.totalResults
+//     console.log(res)
+//     renderMovies(datas, totalRes, title)
+//   } catch (err) {
+//     renderError()
+//   }
+// }
 
 const searchMovies = () => {
   let page = 1
@@ -36,6 +78,7 @@ const searchMovies = () => {
 searchMovies()
 
 const renderMovies = (datas, totalRes, title) => {
+  console.log(datas)
   datas.forEach((data) => {
     const divEl = document.createElement('div')
     const titleEl = document.createElement('div')
@@ -57,9 +100,13 @@ const renderMovies = (datas, totalRes, title) => {
   resultEl.classList.add('resultDiv')
   resultEl.textContent = `${title}의 총 검색 결과: ${totalRes}개`
   sortEl.classList.add('view')
-  
+
   resultEl.appendChild(sortEl)
   mainInnerEl.prepend(resultEl)
+  errorContainerEl.classList.remove('error')
+  errorContainerEl.classList.add('hide')
+  loadingEl.classList.remove('viewImg')
+  loadingEl.classList.add('hideImg')
 }
 
 // validation 구현
